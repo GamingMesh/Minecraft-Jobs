3fbee9367009738490bfce7167c46dba38aa26f8
Zak Ford <zak.j.ford@gmail.com>
2011-06-30 03:18:22 -0400

    Jobs with no kill info will correctly display the kill-none message

--- src/com/zford/jobs/config/JobsConfiguration.java
+++ src/com/zford/jobs/config/JobsConfiguration.java
@@ -474,6 +474,12 @@ public class JobsConfiguration {
                     }
                 }
             }
+            
+            // set to null if there are no kills
+            if(jobKillInfo.size() == 0) {
+                jobKillInfo = null;
+            }
+            
             this.jobs.put(jobName.toLowerCase(), new Job(jobBreakInfo, jobPlaceInfo, jobKillInfo, jobFishInfo, jobName, jobShortName, jobColour, maxExpEquation, incomeEquation, expEquation, displayMethod, maxLevel, maxSlots));
         }
 	}
