c174202c10facbfb5a29dc64f0fbc597a8ed2573
Zak Ford <zak.j.ford@gmail.com>
2012-03-14 20:49:14 -0400

    Minor cleanup of JobsPlayer

--- src/main/java/me/zford/jobs/config/container/JobsPlayer.java
+++ src/main/java/me/zford/jobs/config/container/JobsPlayer.java
@@ -19,11 +19,13 @@
 
 package me.zford.jobs.config.container;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 
 
 import me.zford.jobs.Jobs;
@@ -44,10 +46,8 @@ public class JobsPlayer {
     private Jobs plugin;
 	// the player the object belongs to
 	private String playername;
-	// list of all jobs that the player does
-	private List<Job> jobs;
 	// progression of the player in each job
-	private HashMap<Job, JobProgression> progression;
+	private LinkedHashMap<Job, JobProgression> progression = new LinkedHashMap<Job, JobProgression>();
 	// display honorific
 	private String honorific = null;
 		
@@ -62,23 +62,21 @@ public class JobsPlayer {
 	    this.plugin = plugin;
 		// set player link
 		this.playername = playername;
-		this.jobs = new ArrayList<Job>();
-		this.progression = new HashMap<Job, JobProgression>();
 		// for all jobs players have
 		List<JobsDAOData> list = dao.getAllJobs(this);
-		if(list != null){
-			for(JobsDAOData job: list){
-				if(plugin.getJobConfig().getJob(job.getJobName()) != null){
+		if (list != null) {
+			for (JobsDAOData jobdata: list) {
+				if (plugin.getJobConfig().getJob(jobdata.getJobName()) != null) {
 					// add the job
-					jobs.add(plugin.getJobConfig().getJob(job.getJobName()));
-					
-					// create the progression object
-					JobProgression jobProgression = 
-						new JobProgression(plugin, plugin.getJobConfig().getJob(job.getJobName()), job.getExperience(),job.getLevel(), this);
-					// calculate the max level
-					
-					// add the progression level.
-					progression.put(jobProgression.getJob(), jobProgression);
+				    Job job = plugin.getJobConfig().getJob(jobdata.getJobName());
+					if (job != null) {
+    					// create the progression object
+    					JobProgression jobProgression = new JobProgression(plugin, job, jobdata.getExperience(), jobdata.getLevel(), this);
+    					// calculate the max level
+    					
+    					// add the progression level.
+    					progression.put(job, jobProgression);
+					}
 				}
 			}
 		}
@@ -294,23 +292,23 @@ public class JobsPlayer {
 	 * Get the list of jobs
 	 * @return the list of jobs
 	 */
-	public List<Job> getJobs(){
-		return jobs;
+	public Set<Job> getJobs() {
+		return Collections.unmodifiableSet(progression.keySet());
 	}
 	
 	/**
 	 * Get the list of job progressions
 	 * @return the list of job progressions
 	 */
-	public Collection<JobProgression> getJobsProgression(){
-		return progression.values();
+	public Collection<JobProgression> getJobsProgression() {
+		return Collections.unmodifiableCollection(progression.values());
 	}
 	
 	/**
 	 * Get the job progression with the certain job
 	 * @return the job progression
 	 */
-	public JobProgression getJobsProgression(Job job){
+	public JobProgression getJobsProgression(Job job) {
 		return progression.get(job);
 	}
 	
@@ -341,78 +339,62 @@ public class JobsPlayer {
 		}
 	}
 	
-	public String getDisplayHonorific(){
-		
-		String honorific = "";		
-		
-		if(jobs.size() > 1){
-			// has more than 1 job - using shortname mode
-			for(JobProgression temp: progression.values()){
-				if(temp.getJob().getDisplayMethod().equals(DisplayMethod.FULL) || 
-						temp.getJob().getDisplayMethod().equals(DisplayMethod.TITLE) ||
-						temp.getJob().getDisplayMethod().equals(DisplayMethod.SHORT_FULL) || 
-						temp.getJob().getDisplayMethod().equals(DisplayMethod.SHORT_TITLE)){
-					// add title to honorific
-					if(temp.getTitle() != null){
-						honorific += temp.getTitle().getChatColor() + temp.getTitle().getShortName() + ChatColor.WHITE;
-					}
-				}
-				
-				if(temp.getJob().getDisplayMethod().equals(DisplayMethod.FULL) || 
-						temp.getJob().getDisplayMethod().equals(DisplayMethod.JOB) ||
-						temp.getJob().getDisplayMethod().equals(DisplayMethod.SHORT_FULL) || 
-						temp.getJob().getDisplayMethod().equals(DisplayMethod.SHORT_JOB)){
-					honorific += temp.getJob().getChatColour() + temp.getJob().getShortName() + ChatColor.WHITE;
-				}
-				
-				if(!temp.getJob().getDisplayMethod().equals(DisplayMethod.NONE)){
-					honorific+=" ";
-				}
-			}
-		}
-		else{
-		    Job job;
-		    if(jobs.size() == 0) {
-		        job = plugin.getJobConfig().getJob("None");
-		    } else {
-		        job = jobs.get(0);
-		    }
+	public String getDisplayHonorific() {
+		StringBuilder builder = new StringBuilder();
+		int numJobs = progression.size();
+	    boolean gotTitle = false;
+		for (JobProgression prog: progression.values()) {
+		    DisplayMethod method = prog.getJob().getDisplayMethod();
 		    
-            if(job == null) {
-                return null;
+		    if (method.equals(DisplayMethod.NONE))
+		        continue;
+            
+            if (gotTitle) {
+                builder.append(" ");
+                gotTitle = false;
             }
             
-		    JobProgression jobProgression = progression.get(job);
-		    
-			// using longname mode
-			if(job.getDisplayMethod().equals(DisplayMethod.FULL) || job.getDisplayMethod().equals(DisplayMethod.TITLE)){
+            if (numJobs == 1) {
+                if (method.equals(DisplayMethod.FULL) || method.equals(DisplayMethod.TITLE)) {
+                    if (prog.getTitle() != null) {
+                        builder.append(prog.getTitle().getChatColor() + prog.getTitle().getName() + ChatColor.WHITE);
+                        gotTitle = true;
+                    }
+                }
+                
+                if(method.equals(DisplayMethod.FULL) || method.equals(DisplayMethod.JOB)) {
+                    if (gotTitle) {
+                        builder.append(" ");
+                    }
+                    builder.append(prog.getJob().getChatColour() + prog.getJob().getName() + ChatColor.WHITE);
+                    gotTitle = true;
+                }
+            }
+            
+			if (numJobs > 1 && (method.equals(DisplayMethod.FULL) || method.equals(DisplayMethod.TITLE)) ||
+			        method.equals(DisplayMethod.SHORT_FULL) ||
+			        method.equals(DisplayMethod.SHORT_TITLE)) {
 				// add title to honorific
-				if(jobProgression != null && jobProgression.getTitle() != null){
-					honorific += jobProgression.getTitle().getChatColor() + jobProgression.getTitle().getName() + ChatColor.WHITE;
-				}
-				if(job.getDisplayMethod().equals(DisplayMethod.FULL)){
-					honorific += " ";
-				}
-			}
-			if(job.getDisplayMethod().equals(DisplayMethod.SHORT_FULL) || job.getDisplayMethod().equals(DisplayMethod.SHORT_TITLE)){
-				// add title to honorific
-				if(jobProgression != null && jobProgression.getTitle() != null){
-					honorific += jobProgression.getTitle().getChatColor() + jobProgression.getTitle().getShortName() + ChatColor.WHITE;
+				if (prog.getTitle() != null) {
+				    builder.append(prog.getTitle().getChatColor() + prog.getTitle().getShortName() + ChatColor.WHITE);
+				    gotTitle = true;
 				}
 			}
 			
-			if(job.getDisplayMethod().equals(DisplayMethod.FULL) || job.getDisplayMethod().equals(DisplayMethod.JOB)){
-				honorific += job.getChatColour() + job.getName() + ChatColor.WHITE;
-			}
-			if(job.getDisplayMethod().equals(DisplayMethod.SHORT_FULL) || job.getDisplayMethod().equals(DisplayMethod.SHORT_JOB)){
-				honorific += job.getChatColour() + job.getShortName() + ChatColor.WHITE;
+			if (numJobs > 1 && (method.equals(DisplayMethod.FULL) || method.equals(DisplayMethod.JOB)) ||
+			        method.equals(DisplayMethod.SHORT_FULL) || 
+			        method.equals(DisplayMethod.SHORT_JOB)) {
+			    builder.append(prog.getJob().getChatColour() + prog.getJob().getShortName() + ChatColor.WHITE);
+			    gotTitle = true;
 			}
 		}
 		
-		if(honorific.equals("")){
+		String honorific = builder.toString().trim();
+		
+		if (honorific.isEmpty()) {
 			return null;
-		}else{
-			return honorific.trim();
+		} else {
+			return honorific;
 		}
 	}
 	
@@ -420,8 +402,7 @@ public class JobsPlayer {
 	 * Player joins a job
 	 * @param job - the job joined
 	 */
-	public void joinJob(Job job){
-		jobs.add(job);
+	public void joinJob(Job job) {
 		progression.put(job, new JobProgression(plugin, job, 0.0, 1, this));
 	}
 	
@@ -429,8 +410,7 @@ public class JobsPlayer {
 	 * Player leaves a job
 	 * @param job - the job left
 	 */
-	public void leaveJob(Job job){
-		jobs.remove(job);
+	public void leaveJob(Job job) {
 		progression.remove(job);
 	}
 	
@@ -438,14 +418,10 @@ public class JobsPlayer {
 	 * Player leaves a job
 	 * @param job - the job left
 	 */
-	public void transferJob(Job oldjob, Job newjob){
-		JobProgression prog = progression.get(oldjob);
-		jobs.remove(oldjob);
-		jobs.add(newjob);
-		progression.remove(oldjob);
+	public void transferJob(Job oldjob, Job newjob) {
+		JobProgression prog = progression.remove(oldjob);
 		prog.setJob(newjob);
 		progression.put(newjob, prog);
-		
 	}
 	
 	/**
@@ -454,30 +430,28 @@ public class JobsPlayer {
 	 * @return true - they are in the job
 	 * @return false - they are not in the job
 	 */
-	public boolean isInJob(Job job){
-		return jobs.contains(job);
+	public boolean isInJob(Job job) {
+		return progression.containsKey(job);
 	}
 	
 	/**
 	 * Function that reloads your honorific
 	 */
-	public void reloadHonorific(){
+	public void reloadHonorific() {
 		String newHonorific = getDisplayHonorific();
         Player player = plugin.getServer().getPlayer(playername);
         if(player == null)
             return;
 		
-		if(newHonorific == null && honorific != null){
+		if (newHonorific == null && honorific != null) {
 			// strip the current honorific.
-		    player.setDisplayName(player.getDisplayName().trim().replaceFirst(honorific + " ", "").trim());
-		}
-		else if (newHonorific != null && honorific != null){
+		    player.setDisplayName(player.getDisplayName().replaceFirst(honorific + " ", ""));
+		} else if (newHonorific != null && honorific != null) {
 			// replace the honorific
-		    player.setDisplayName(player.getDisplayName().trim().replaceFirst(honorific, newHonorific).trim());
-		}
-		else if(newHonorific != null && honorific == null){
+		    player.setDisplayName(player.getDisplayName().replaceFirst(honorific, newHonorific));
+		} else if (newHonorific != null && honorific == null) {
 			// new honorific
-		    player.setDisplayName((newHonorific + " " + player.getDisplayName().trim()).trim());
+		    player.setDisplayName((newHonorific + " " + player.getDisplayName()));
 		}
 		// set the new honorific
 		honorific = newHonorific;
@@ -490,8 +464,8 @@ public class JobsPlayer {
         Player player = plugin.getServer().getPlayer(playername);
         if(player == null)
             return;
-        if(honorific != null) {
-            player.setDisplayName(player.getDisplayName().trim().replaceFirst(honorific + " ", "").trim());
+        if (honorific != null) {
+            player.setDisplayName(player.getDisplayName().replaceFirst(honorific + " ", ""));
         }
         honorific = null;
     }
@@ -499,12 +473,12 @@ public class JobsPlayer {
 	/**
 	 * Function to reload all of the maximum experiences
 	 */
-	public void reloadMaxExperience(){
+	public void reloadMaxExperience() {
 		HashMap<String, Double> param = new HashMap<String, Double>();
 		param.put("numjobs", (double) progression.size());
-		for(JobProgression temp: progression.values()){
-			param.put("joblevel", (double) temp.getLevel());
-			temp.setMaxExperience((int)temp.getJob().getMaxExp(param));
+		for (JobProgression prog: progression.values()) {
+			param.put("joblevel", (double) prog.getLevel());
+			prog.setMaxExperience((int)prog.getJob().getMaxExp(param));
 			param.remove("joblevel");
 		}
 	}
